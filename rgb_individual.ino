44cc78eb0fa3f6665efbb913437d3c5eef561bad
gremlin88 <mydisposable164@gmail.com>
2015-03-15 20:39:16 -0500

    Version 2
    This version has more comments which are (hopefully) easier to
    understand than before. I have yet to test this code.

--- rgb_individual.ino
+++ rgb_individual.ino
@@ -1,13 +1,13 @@
-int R_in = 2;  //channel numbers and variables
-int G_in = 3;
-int B_in = 4;
-int R_out = 5;
-int G_out = 6;
-int B_out = 7;
-int R_val;
+const int R_in = 2;  // input channel numbers
+const int G_in = 3;
+const int B_in = 4;
+const int R_out = 5;  //output channel numbers
+const int G_out = 6;
+const int B_out = 7;
+int R_val;  //variables to store input values
 int G_val;
 int B_val;
-int R_val_out;
+int R_val_out;  //variables to store output values
 int G_val_out;
 int B_val_out;
 
@@ -28,8 +28,8 @@ void loop()
   G_val = pulseIn(G_in, HIGH);
   B_val = pulseIn(B_in, HIGH);
   //convert pulse width into pwm
-  R_val_out = map(R_val, 1000, 2000, -372.5, 627.5);
-  R_val_out = constrain(R_val_out, 0, 255);
+  R_val_out = map(R_val, 1000, 2000, -372.5, 627.5);  //Center the input values
+  R_val_out = constrain(R_val_out, 0, 255);  //constrain the new values to passable PWM values
   //convert pulse width into pwm
   G_val_out = map(G_val, 1000, 2000, -372.5, 627.5);
   G_val_out = constrain(G_val_out, 0, 255);
@@ -37,7 +37,7 @@ void loop()
   B_val_out = map(B_val, 1000, 2000, -372.5, 627.5);
   B_val_out = constrain(B_val_out, 0, 255);
   //output analog signal
-  analogWrite(R_out, R_val_out);
+  analogWrite(R_out, R_val_out);  //output the new values
   analogWrite(G_out, G_val_out);
   analogWrite(B_out, B_val_out);
 }
